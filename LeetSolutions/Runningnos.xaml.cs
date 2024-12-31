f7c1092ecff5fe20036c4b8ace724e037d1a4839
Pavithra214 <pavithra.it214@gmail.com>
2024-12-31 12:46:44 +0530

    Leet solutions

---
+++ LeetSolutions/Runningnos.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+
+namespace LeetSolutions
+{
+    /// <summary>
+    /// Interaction logic for Runningnos.xaml
+    /// </summary>
+    public partial class Runningnos : Window
+    {
+        public Runningnos()
+        {
+            InitializeComponent();
+            int a,b,c;
+        }
+      
+
+        private void problmsolving_Click(object sender, RoutedEventArgs e)
+        {
+            string s = "Hello World";
+            int last = s.LastIndexOf(" ");
+            string result = s.Substring(last);
+            int cont = result.Length;
+            cont--;
+            MessageBox.Show(cont.ToString());
+        }
+    }
+}
